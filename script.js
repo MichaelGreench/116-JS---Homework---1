020b8d64fee561ee8c18da4d0f8c0843f8b46d32
MichaelGreench <106013652+MichaelGreench@users.noreply.github.com>
2022-07-20 23:49:40 +0400

    JS-HomeWork1

---
+++ script.js
@@ -0,0 +1,98 @@
+
+// დავალება №1
+
+// let user = {
+//     firstName:'Michael',
+//     lastName: 'Grigoryan',
+//     studentAge: 35,
+//     studentStatus: 'active',
+// };
+
+// console.log(user.studentStatus);
+
+// დავალება №2
+
+// var num = [5, 6, 10, 25];
+// console.log(num[0]);
+// console.log(num[1]);
+// console.log(num[2]);
+// console.log(num[3]);
+
+// დავალება №3
+
+// var numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+// for(var i=0; i<numbers.length; i++){
+//     if(numbers[i]>5){
+//         console.log(numbers[i])
+//     }
+// };
+
+// დავალება №4
+
+// var user = {
+//     Name: 'Giorgi',
+//     Age: 20,
+//     studentStatus: 'active',
+// }
+// if(user.Age <= 20 && user.studentStatus == 'active'){
+//     console.log('hello');
+// }
+// else if(user.name == 'Levani'){
+//     console.log('Hello Giorgi');
+// }
+// else if(user.studentStatus == 'active' || user.age < 25){
+//     console.log('Hello World');
+// }
+// else{
+//     console.log('error');
+// }
+
+// დავალება №5
+//     var array = [ 'Peach', 'Apple', 10, 45, 50, 'Banana', 'Avocado' ] ;
+//     for(i=0; i<array.length; i++){
+//         if(typeof array[i] === 'string'){
+//             console.log(array[i]);
+//         }
+//     }
+
+// დავალება №6
+
+// var array = [32, 10, 'hello', null, 'hello2', 50 ]
+// for (let item of array){
+//     if (item % 5 == 0 && typeof item == 'number')
+//     console.log(item);
+
+// }
+
+// დავალება №7
+
+// var array = [ [2, -3, 5, 10], [25, -24, -11, 100] , [-6, -7, 10] ];
+// for (let i = 0;  i < array.length; i++) {
+//     const num = array[i];
+//         for (let x =0; x < num.length; x++) {
+//         const positive = num[x];
+//         if(positive > 0)
+//         console.log(positive);
+        
+//     }
+    
+// }
+
+// დამატებითი დავალება: 1
+
+// var array = [ 2, 3, 5, 10, 25, 24, 11, 100, 6, 7, 10 ];
+// for(let x of array){
+//     if(x % 2 == 0){
+//         console.log(x);
+//     }
+// }
+
+// დამატებითი დავალება: 2
+
+// var array = [ 2, 3, 5, 10, 25, 24, 11, 100, 6, 7, 10 ];
+// for(let x of array){
+//     if(x % 2 == 1){
+//         console.log(x);
+//     }
+// }
+    
